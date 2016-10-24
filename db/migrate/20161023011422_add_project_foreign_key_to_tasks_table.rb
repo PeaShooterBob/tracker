041dba87f96a3f59200e8766f1cf8008ced5a5e3
peashooterbob <rgu@8thlight.com>
2016-10-24 10:00:42 -0700

    Task id is a uuid

--- db/migrate/20161023011422_add_project_foreign_key_to_tasks_table.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectForeignKeyToTasksTable < ActiveRecord::Migration
-  def change
-    add_column :tasks, :project_id, :uuid
-  end
-end
--- db/migrate/20161023011751_add_project_index_to_tasks_table.rb
+++
@@ -1,5 +0,0 @@
-class AddProjectIndexToTasksTable < ActiveRecord::Migration
-  def change
-    add_index :tasks, :project_id
-  end
-end
